34f56a990e05d1f43bcd906f57787d3e01a21f6a
Your Name <you@example.com>
2026-05-01 09:36:21 +0200

    update insert sort and add insert sort with list example

--- insert_sort.c
+++ insert_sort.c
@@ -1,40 +1,56 @@
 // C program for implementation of Insertion Sort
 #include <stdio.h>
 
-/* Function to sort array using insertion sort */
-void insertionSort(int arr[], int n)
+//insert sort function
+void insert_sort(int arr[], int len_arr)
 {
-    for (int i = 1; i < n; ++i) {
-        int key = arr[i];
-        int j = i - 1;
+	int	key;//compare with previous int and keep arr[i] value
+	int	i;//for first while loop to compare each key
+	int	j;//for the second while loop to compare sorted elements
 
-        /* Move elements of arr[0..i-1], that are
-           greater than key, to one position ahead
-           of their current position */
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+	i = 1;
+	while (i < len_arr)
+	{
+		key = arr[i];
+		j = i - 1;
+		while (arr[j] > key && j >= 0)
+		{
+			// arr[j] = arr[i];//why this is wrong?
+			// arr[i] = arr[j];//why this is wrong?
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		// arr[i] = arr[i - 1];//why this is wrong?
+		arr[j + 1] = key;
+		i++;
+	}
 }
 
-/* A utility function to print array of size n */
-void printArray(int arr[], int n)
+//print array function
+void	print_arr(int arr[], int len_arr)
 {
-    for (int i = 0; i < n; ++i)
-        printf("%d ", arr[i]);
-    printf("\n");
+	int	i;
+
+	i = 0;
+	while (i < len_arr)
+	{
+		printf("%d ", arr[i]);
+		i++;
+	}
 }
 
-// Driver method
-int main()
+//entry point for testing
+int main(void)
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int n = sizeof(arr) / sizeof(arr[0]);
+	int arr[] = {7, 6, 5, 4, 3, 2, 1, 0};
+	int	len_arr;
 
-    insertionSort(arr, n);
-    printArray(arr, n);
-
-    return 0;
+	len_arr = sizeof(arr) / sizeof(arr[0]);
+	printf("before: ");
+	print_arr(arr, len_arr);
+	printf("\n");
+	insert_sort(arr, len_arr);
+	printf("after: ");
+	print_arr(arr, len_arr);
+	return 0;
 }
